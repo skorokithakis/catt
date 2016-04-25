6eea06fcdf05145bdfabccca8780028629796b6f
Stavros Korokithakis <stavros@korokithakis.net>
2016-04-25 05:07:38 +0300

    test: Fix tests.

--- tests/test_catt.py
+++ tests/test_catt.py
@@ -4,20 +4,24 @@
 import time
 import unittest
 
-from catt import catt
+from catt.controllers import Cache, get_stream_url
 
 
 class TestThings(unittest.TestCase):
     def test_get_stream_url(self):
-        url = catt.get_stream_url("https://www.youtube.com/watch?v=VZMfhtKa-wo")
+        url = get_stream_url("https://www.youtube.com/watch?v=VZMfhtKa-wo")
         self.assertIn("https://", url)
 
     def test_cache(self):
-        cache = catt.Cache("/tmp/catt_cache/")
+        cache = Cache()
         cache.set("key", "value")
         self.assertEqual(cache.get("key", 1), "value")
         time.sleep(1.2)
         self.assertIsNone(cache.get("key", 1))
+
+        cache.set("key", "value")
+        cache.clear()
+        self.assertIsNone(cache.get("key", 100))
 
 
 if __name__ == '__main__':
