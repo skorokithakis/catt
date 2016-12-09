12439d0ad6e568553fb49bc99ae713076985bd37
theychx <theychx@users.noreply.github.com>
2016-12-09 17:14:54 +0100

    Add device choice (#1)
    * feat: Add user selectable CC device
    
    * fix: Stabilize cache handling
    
    * fix: Make test_catt.py pass
    
    * fix: Slight improvement of cache
    
    * fix: Make test_catt.py pass (again)
    
    * fix: Solution for edge case where cc would not be auto-discovered
    
    * fix: Weed out another auto-discovery edge case
    
    * fix: Simplify cache retrieval
    
    * fix: Change cache filename and fix tempdir

--- tests/test_catt.py
+++ tests/test_catt.py
@@ -15,13 +15,17 @@ class TestThings(unittest.TestCase):
     def test_cache(self):
         cache = Cache()
         cache.set("key", "value")
-        self.assertEqual(cache.get("key", 1), "value")
+        self.assertEqual(cache.get("key"), "value")
+
         time.sleep(1.2)
-        self.assertIsNone(cache.get("key", 1))
+        cache = Cache(duration=1)
+        self.assertEqual(cache.get("key"), None)
 
         cache.set("key", "value")
         cache.clear()
-        self.assertIsNone(cache.get("key", 100))
+        cache = Cache()
+        self.assertEqual(cache.get("key"), None)
+        cache.clear()
 
 
 if __name__ == '__main__':
