928369fd7e4c0d8cdc7323e452baaad9e24bb651
theychx <boomingzooming@gmail.com>
2017-03-19 10:32:03 +0100

    fix: Make check_state private

--- catt/controllers.py
+++ catt/controllers.py
@@ -146,7 +146,7 @@ class CastController:
 
         self.cast.wait()
 
-    def check_state(self):
+    def _check_state(self):
         if self.cast.app_id == "E8C28D3C" or not self.cast.app_id:
             raise CattCastError("Chromecast is inactive.")
         self.cast.media_controller.block_until_active(1.0)
@@ -158,28 +158,28 @@ class CastController:
         self.cast.media_controller.block_until_active()
 
     def play(self):
-        self.check_state()
+        self._check_state()
         self.cast.media_controller.play()
 
     def pause(self):
-        self.check_state()
+        self._check_state()
         self.cast.media_controller.pause()
 
     def stop(self):
-        self.check_state()
+        self._check_state()
         self.cast.media_controller.stop()
 
     def seek(self, seconds):
-        self.check_state()
+        self._check_state()
         self.cast.media_controller.seek(seconds)
 
     def rewind(self, seconds):
-        self.check_state()
+        self._check_state()
         pos = self.cast.media_controller.status.current_time
         self.cast.media_controller.seek(pos - seconds)
 
     def ffwd(self, seconds):
-        self.check_state()
+        self._check_state()
         pos = self.cast.media_controller.status.current_time
         self.cast.media_controller.seek(pos + seconds)
 
@@ -193,7 +193,7 @@ class CastController:
         self.cast.volume_down()
 
     def status(self):
-        self.check_state()
+        self._check_state()
         status = self.cast.media_controller.status.__dict__
 
         if not status["duration"]:
@@ -212,7 +212,7 @@ class CastController:
         )
 
     def info(self):
-        self.check_state()
+        self._check_state()
         status = self.cast.media_controller.status.__dict__
         for (key, value) in status.items():
             echo("%s : %s" % (key, value))
