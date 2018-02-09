a7176876d845b4c1bebee3a1edeaef3721934356
theychx <boomingzooming@gmail.com>
2018-02-09 01:04:01 +0100

    Fix grammar mistake

--- catt/stream_info.py
+++ catt/stream_info.py
@@ -49,7 +49,7 @@ class StreamInfo:
             if self.is_playlist:
                 items = list(self._preinfo["entries"])
                 self._first_entry_info = self._get_stream_info(items[0])
-                # Some playlist extractors does not provide an id key with entries,
+                # Some playlist extractors do not provide an id key with entries,
                 # so we need to tolerate that. _playlist_items is only required by
                 # custom controllers anyway.
                 try:
