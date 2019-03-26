98335de4b87638eff9613279bdd106651d4aefe1
Stavros Korokithakis <hi@stavros.io>
2019-03-26 15:38:13 +0200

    fix: Make catt refuse to install under 2 more

--- catt/__init__.py
+++ catt/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
+if sys.version_info.major < 3:
+    print("This program requires Python 3 and above to run.")
+    sys.exit(1)
+
 __author__ = "Stavros Korokithakis"
 __email__ = "hi@stavros.io"
 __version__ = "0.9.3"
