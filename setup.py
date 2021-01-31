6431053b3f418107d27973a491d373306a72187f
Stavros Korokithakis <hi@stavros.io>
2021-01-31 17:06:13 +0200

    Specify higher PyChromecast version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.rst") as readme_file:
 
 requirements = [
     "youtube-dl>=2020.9.20",
-    "PyChromecast>=7.5.0",
+    "PyChromecast>=8.0.0",
     "Click>=7.1.2,<8",
     "ifaddr>=0.1.7",
     "requests>=2.23.0",
