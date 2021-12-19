b3b76d6c70db7cb07e394fe19179b92620ebceca
Stavros Korokithakis <hi@stavros.io>
2021-12-19 20:36:05 +0200

    Ughhhhhhhhhh

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
