1c194000e2401b097b3c55dae885782b04d34bc8
Stavros Korokithakis <hi@stavros.io>
2022-01-15 04:45:04 +0200

    Remove leftovers

--- catt/__init__.py
+++ catt/__init__.py
@@ -5,7 +5,4 @@ if sys.version_info.major < 3:
     print("This program requires Python 3 and above to run.")
     sys.exit(1)
 
-__author__ = "Stavros Korokithakis"
-__email__ = "hi@stavros.io"
-__version__ = "0.12.5"
 __codename__ = "Zaniest Zapper"
